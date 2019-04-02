1c7eb57bbabf3764fa545525b5ec476ec6957069
Iwasaki Shuzo <shu.works.7@gmail.com>
2019-04-03 00:37:57 +0900

    rename

--- JumpToHint/Properties/AssemblyInfo.cs
+++ JumpToHint/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VSJumpy")]
+[assembly: AssemblyTitle("JumpToHint")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VSJumpy")]
+[assembly: AssemblyProduct("JumpToHint")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
